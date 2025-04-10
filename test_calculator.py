3e223218a1a6b9c4e176d5e7afa437815fd0947f
sotoya99 <s.otoya@ufl.edu>
2025-04-09 21:59:48 -0400

    modified test_calculator

--- test_calculator.py
+++ test_calculator.py
@@ -1,6 +1,10 @@
 import unittest
 from calculator import *
 
+#https://github.com/sotoya99/lab10-SO-CH
+#Partner 1: Santiago Otoya
+#Partner 2: Cher Huang
+
 class TestCalculator(unittest.TestCase):
     # Partner 2
     def test_add(self): # 3 assertions
@@ -12,10 +16,14 @@ class TestCalculator(unittest.TestCase):
 
     # Partner 1
     def test_multiply(self): # 3 assertions
-        pass
+        self.assertEqual(mul(3, 4), 12)
+        self.assertEqual(mul(-2, 5), -10)
+        self.assertEqual(mul(0, 100), 0)
 
     def test_divide(self): # 3 assertions
-        pass
+        self.assertEqual(div(2, 10), 5)
+        self.assertEqual(div(4, 8), 2)
+        self.assertEqual(div(-2, -10), 5)
     ##########################
 
     # Partner 2
@@ -38,17 +46,22 @@ class TestCalculator(unittest.TestCase):
         # call log function inside, example:
         # with self.assertRaises(<INSERT_ERROR_TYPE>):
         #     logarithm(0, 5)
-        pass
+        self.assertIsNone(logarithm(-5, 2))
 
     def test_hypotenuse(self): # 3 assertions
-        pass
+        self.assertAlmostEqual(hypotenuse(3, 4), 5)
+        self.assertAlmostEqual(hypotenuse(5, 12), 13)
+        self.assertAlmostEqual(hypotenuse(0, 0), 0)
 
     def test_sqrt(self): # 3 assertions
         # Test for invalid argument, example:
         # with self.assertRaises(<INSERT_ERROR_TYPE>):
         #    square_root(NUM)
         # Test basic function
-        pass
+        self.assertEqual(square_root(4), 2)
+        self.assertEqual(square_root(0), 0)
+        with self.assertRaises(ValueError):
+            square_root(-1)
     ##########################
 
 # Do not touch this
